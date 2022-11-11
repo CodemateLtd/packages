910f7620e234933f232da1fc4561cb52760ec230
Juho Rautio <juho.rautio@codemate.com>
2022-11-11 16:24:01 +0200

    Routes response base type

--- packages/google_maps_routes_api/lib/src/types/routes_response.dart
+++ packages/google_maps_routes_api/lib/src/types/routes_response.dart
@@ -1,3 +1,30 @@
+import 'package:google_maps_routes_api/src/types/route.dart';
 export 'package:google_maps_routes_api/src/types/toll_info.dart';
 export 'package:google_maps_routes_api/src/types/route.dart';
 export 'package:google_maps_routes_api/src/types/route_leg.dart';
+
+class RoutesResponse {
+  const RoutesResponse({ this.routes, this.fallbackInfo });
+
+  final List<Route>? routes;
+  final FallbackInfo? fallbackInfo;
+}
+
+class FallbackInfo {
+  const FallbackInfo({ this.routingMode, this.reason });
+
+  final FallbackRoutingMode? routingMode;
+  final FallbackReason? reason;
+}
+
+enum FallbackRoutingMode {
+  FALLBACK_ROUTING_MODE_UNSPECIFIED,
+  FALLBACK_TRAFFIC_UNAWARE,
+  FALLBACK_TRAFFIC_AWARE,
+}
+
+enum FallbackReason {
+  FALLBACK_REASON_UNSPECIFIED,
+  SERVER_ERROR,
+  LATENCY_EXCEEDED,
+}
