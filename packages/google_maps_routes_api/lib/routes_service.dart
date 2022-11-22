a5ebeac1b6291f932535d7fc6790f2a1377baf9e
Juho Rautio <juho.rautio@codemate.com>
2022-11-22 17:58:27 +0200

    Add routes api calls

---
+++ packages/google_maps_routes_api/lib/routes_service.dart
@@ -0,0 +1,65 @@
+library google_maps_routes_api;
+
+import 'dart:convert';
+
+import 'package:google_maps_routes_api/src/types/route_matrix_request.dart';
+import 'package:google_maps_routes_api/src/types/route_matrix.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:google_maps_routes_api/src/types/routes_request.dart';
+import 'package:google_maps_routes_api/src/types/routes_response.dart';
+
+/// A service used to calculate a route between two points
+class RoutesService {
+  const RoutesService({required this.apiKey});
+
+  final String apiKey;
+
+  static const _routesApiUrl = 'https://routes.googleapis.com/';
+
+  Future<RoutesResponse> computeRoute(
+    RoutesRequest body, {
+    String? fields,
+    Map<String, String>? headers,
+  }) async {
+    const url = '$_routesApiUrl/directions/v2:computeRoutes';
+    Map<String, String> defaultHeaders = {
+      'X-Goog-Api-Key': apiKey,
+      'X-Goog-Fieldmask': fields ?? 'routes.duration, routes.distanceMeters',
+      "Content-Type": "application/json",
+    };
+
+    final response = await http.post(Uri.parse(url),
+        body: jsonEncode(body.toJson()),
+        headers: {...defaultHeaders, ...?headers});
+
+    if (response.statusCode != 200) {
+      throw Exception(response.body);
+    }
+
+    final result = RoutesResponse.fromJson(json.decode(response.body));
+    return Future<RoutesResponse>.value(result);
+  }
+
+  Future<List<RouteMatrix>> computeRouteMatrix(RouteMatrixRequest body,
+      {String? fields, Map<String, String>? headers}) async {
+    const url = '$_routesApiUrl/distanceMatrix/v2:computeRouteMatrix';
+    Map<String, String> defaultHeaders = {
+      'X-Goog-Api-Key': apiKey,
+      'X-Goog-Fieldmask': fields ?? 'routes.duration, routes.distanceMeters',
+      "Content-Type": "application/json",
+    };
+
+    final response = await http.post(Uri.parse(url),
+        body: jsonEncode(body.toJson()),
+        headers: {...defaultHeaders, ...?headers});
+
+    if (response.statusCode != 200) {
+      throw Exception(response.body);
+    }
+
+    List<RouteMatrix> result = List<RouteMatrix>.from(
+        json.decode(response.body).map((model) => RouteMatrix.fromJson(model)));
+    return Future<List<RouteMatrix>>.value(result);
+  }
+}
