48284e3d6c91590055c826b74c6f3cd3a015fb85
Juho Rautio <juho.rautio@codemate.com>
2022-11-22 17:29:31 +0200

    Route matrix types

---
+++ packages/google_maps_routes_api/lib/src/types/route_matrix_request.dart
@@ -0,0 +1,66 @@
+import 'package:google_maps_routes_api/src/types/enums.dart';
+import 'package:google_maps_routes_api/src/types/location.dart';
+import 'package:google_maps_routes_api/src/types/routes_request.dart';
+
+class RouteMatrixRequest {
+  RouteMatrixRequest({
+    required this.origins,
+    required this.destinations,
+    this.travelMode,
+    this.routingPreference,
+    this.departureTime,
+  });
+
+  final List<RouteMatrixOrigin> origins;
+  final List<RouteMatrixDestination> destinations;
+  RouteTravelMode? travelMode;
+  RoutingPreference? routingPreference;
+  String? departureTime;
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> json = <String, dynamic>{
+      'origins': origins.map((origin) => origin.toJson()).toList(),
+      'destinations': destinations.map((destination) => destination.toJson()).toList(),
+      'travelMode': travelMode?.name,
+      'routingPreference': routingPreference?.name,
+      'departureTime': departureTime,
+    };
+
+    json.removeWhere((key, value) => value == null);
+    return json;
+  }
+}
+
+class RouteMatrixOrigin {
+  RouteMatrixOrigin({
+    required this.waypoint,
+    this.routeModifiers,
+  });
+
+  final Waypoint waypoint;
+  RouteModifiers? routeModifiers;
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> json = <String, dynamic>{
+      'waypoint': waypoint.toJson(),
+      'routeModifiers': routeModifiers?.toJson(),
+    };
+
+    json.removeWhere((key, value) => value == null);
+    return json;
+  }
+}
+
+class RouteMatrixDestination {
+  RouteMatrixDestination({
+    required this.waypoint,
+  });
+
+  final Waypoint waypoint;
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'waypoint': waypoint.toJson(),
+    };
+  }
+}
