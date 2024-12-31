bbcf93d7a382e457092a79d390a8bb818bf38127
Ville Välimaa <ville.valimaa@codemate.com>
2024-12-31 11:35:00 +0800

    refactor: move image tools to separate googlemaputils class

---
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/GoogleMapUtils.h
@@ -0,0 +1,17 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Flutter/Flutter.h>
+#import <GoogleMaps/GoogleMaps.h>
+#import "messages.g.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GoogleMapUtils: NSObject
++ (UIImage *)iconFromBitmap:(FGMPlatformBitmap *)platformBitmap
+                  registrar:(NSObject<FlutterPluginRegistrar> *)registrar
+                screenScale:(CGFloat)screenScale;
+@end
+
+NS_ASSUME_NONNULL_END
