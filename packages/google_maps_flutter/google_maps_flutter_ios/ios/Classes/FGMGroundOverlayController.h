45f64747c0bf12ee6f7424248c1e9c05b6366ace
Ville Välimaa <ville.valimaa@codemate.com>
2025-01-15 18:28:43 +0800

    fix: ios analysis fixes

--- packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/FGMGroundOverlayController.h
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/FGMGroundOverlayController.h
@@ -11,7 +11,7 @@
 @interface FGMGroundOverlayController : NSObject
 
 /// The ground overlay this controller handles.
-@property(strong, nonatomic) GMSGroundOverlay *groundOverlay;
+@property(strong, nonatomic) GMSGroundOverlay * _Nonnull groundOverlay;
 
 /// Whether ground overlay is created with bounds or position.
 @property(nonatomic, assign) BOOL isCreatedWithBounds;
@@ -20,9 +20,9 @@
 @property(nonatomic, strong, nullable) NSNumber *zoomLevel;
 
 /// Initializes an instance of this class with a GMSGroundOverlay, a map view, and identifier.
-- (instancetype)initWithGroundOverlay:(GMSGroundOverlay *)groundOverlay
-                           identifier:(NSString *)identifier
-                              mapView:(GMSMapView *)mapView
+- (instancetype _Nullable )initWithGroundOverlay:(GMSGroundOverlay *_Nonnull)groundOverlay
+                           identifier:(NSString *_Nonnull)identifier
+                              mapView:(GMSMapView *_Nonnull)mapView
                   isCreatedWithBounds:(BOOL)isCreatedWithBounds;
 
 /// Removes this ground overlay from the map.
@@ -33,25 +33,25 @@
 @interface FLTGroundOverlaysController : NSObject
 
 /// Initializes the controller with a GMSMapView, callback handler and registrar.
-- (instancetype)initWithMapView:(GMSMapView *)mapView
-                callbackHandler:(FGMMapsCallbackApi *)callbackHandler
-                      registrar:(NSObject<FlutterPluginRegistrar> *)registrar;
+- (instancetype _Nullable )initWithMapView:(GMSMapView *_Nonnull)mapView
+                           callbackHandler:(FGMMapsCallbackApi *_Nonnull)callbackHandler
+                                 registrar:(NSObject<FlutterPluginRegistrar> *_Nonnull)registrar;
 
 /// Adds ground overlays to the map.
-- (void)addGroundOverlays:(NSArray<FGMPlatformGroundOverlay *> *)groundOverlaysToAdd;
+- (void)addGroundOverlays:(NSArray<FGMPlatformGroundOverlay *> *_Nonnull)groundOverlaysToAdd;
 
 /// Updates ground overlays on the map.
-- (void)changeGroundOverlays:(NSArray<FGMPlatformGroundOverlay *> *)groundOverlaysToChange;
+- (void)changeGroundOverlays:(NSArray<FGMPlatformGroundOverlay *> *_Nonnull)groundOverlaysToChange;
 
 /// Removes ground overlays from the map.
-- (void)removeGroundOverlaysWithIdentifiers:(NSArray<NSString *> *)identifiers;
+- (void)removeGroundOverlaysWithIdentifiers:(NSArray<NSString *> *_Nonnull)identifiers;
 
 /// Called when a ground overlay is tapped on the map.
-- (void)didTapGroundOverlayWithIdentifier:(NSString *)identifier;
+- (void)didTapGroundOverlayWithIdentifier:(NSString *_Nonnull)identifier;
 
 /// Returns true if a ground overlay with the given identifier exists on the map.
-- (bool)hasGroundOverlaysWithIdentifier:(NSString *)identifier;
+- (bool)hasGroundOverlaysWithIdentifier:(NSString *_Nonnull)identifier;
 
 /// Returns FGMPlatformGroundOverlay for identifier.
-- (nullable FGMPlatformGroundOverlay *)groundOverlayWithIdentifier:(NSString *)identifier;
+- (nullable FGMPlatformGroundOverlay *)groundOverlayWithIdentifier:(NSString *_Nonnull)identifier;
 @end
--- packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/google_maps_flutter_ios-umbrella.h
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/google_maps_flutter_ios-umbrella.h
@@ -9,6 +9,9 @@
 #import <google_maps_flutter_ios/FLTGoogleMapTileOverlayController.h>
 #import <google_maps_flutter_ios/FLTGoogleMapsPlugin.h>
 #import <google_maps_flutter_ios/GoogleMapPolylineController_Test.h>
+#import <google_maps_flutter_ios/FGMUtils.h>
+#import <google_maps_flutter_ios/FGMGroundOverlayController_Test.h>
+#import <google_maps_flutter_ios/FGMGroundOverlayController.h>
 
 FOUNDATION_EXPORT double google_maps_flutterVersionNumber;
 FOUNDATION_EXPORT const unsigned char google_maps_flutterVersionString[];
