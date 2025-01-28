7d48344b587bf7940a7029cb6a0cee89546b0001
Alexander Troshkov <20989940+aednlaxer@users.noreply.github.com>
2025-01-28 12:34:25 +0200

    Tweak imports

--- packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/FGMGroundOverlayController.h
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/FGMGroundOverlayController.h
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 #import <Flutter/Flutter.h>
 #import <GoogleMaps/GoogleMaps.h>
 
